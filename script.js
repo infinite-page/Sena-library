2e8ee252639d343b30ca78f0ec37e5473459a901
JhoonHe <jjhonherrera113@gmail.com>
2024-06-04 15:54:42 -0500

    Advance in the library (books)#1

---
+++ script.js
@@ -0,0 +1,212 @@
+let books = [
+
+
+    { 
+
+        title: 'Little prince',
+        author: 'Antoine de Saint-Exupéry',
+        gender: 'narrative literary genre', 
+        language: 'English',
+        price: 20000,
+        format: 'ebook',
+        isbn: '9109109', 
+        description: 'is a timeless tale about a young prince who explores the universe, learning profound lessons about love, friendship, and the human condition along the way', 
+        status: 'new', 
+        location: 'Sena Comercio y Turismo', 
+        publication_date: 'April 6, 1943', 
+        editorial: 'Reynal & Hitchcock', 
+        pages: 100
+
+},
+
+    {
+    
+        title: 'To kill a Monckingbird', 
+        author: 'Harper Lee',
+        gender: 'Narrative Literary',
+        language: 'English',
+        price: 30000, 
+        format: 'ebook',
+        isbn: '9780451524935',
+        description: 'A classic novel set in the American South, exploring themes of racial injustice and moral growth through the eyes of a young girl',
+        status: 'new',
+        location: 'Sena Comercio y Turismo', 
+        publication_date: 'AJuly 11, 1960',
+        editorial: 'J. B. Lippincott & Co.',
+        pages: 300
+                
+    },
+
+    {
+        title: 'Pride and Prejudice',
+        author: 'Jane Austen',
+        genre: 'Romance, Comedy of Manners',
+        language: 'English',
+        price: 30000,
+        format: 'ebook',
+        isbn: '9780141439518',
+        description: 'A witty and charming novel following the romantic entanglements of the Bennet sisters in early 19th-century England',
+        status: 'new',
+        location: 'Sena Comercio y Turismo',
+        publication_date: 'January 28, 1813',
+        editorial: 'Penguin Classics',
+        pages: 368
+      },
+
+
+      {
+        title: 'Harry Potter and the Sorcerer"s Stone',
+        author: 'J.K. Rowling',
+        genre: 'Fantasy',
+        language: 'English',
+        price: 30000,
+        format: 'ebook',
+        isbn: '9780747532743',
+        description: 'The first book in the beloved series follows young Harry Potter as he discovers his magical heritage and begins his journey at Hogwarts School of Witchcraft and Wizardry',
+        status: 'new',
+        location: 'Sena Comercio y Turismo',
+        publication_date: 'June 26, 1997',
+        editorial: 'Bloomsbury Publishing',
+        pages: 223
+      },
+
+
+
+      {
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        genre: 'Tragedy, Jazz Age',
+        language: 'English',
+        price: 40000,
+        format: 'ebook',
+        isbn: '9780743273565',
+        description: 'A mesmerizing tale of love, wealth, and ambition set against the backdrop of the Roaring Twenties in America.',
+        status: 'new',
+        location: 'Sena Comercio y Turismo',
+        publication_date: 'April 10, 1925',
+        editorial: 'Scribner',
+        pages: 180
+      },
+
+
+      {
+        Title: 'The Hobbit',
+        Author: 'J.R.R. Tolkien',
+        Genre: 'Fantasy, Adventure',
+        Language: 'English',
+        Price: 60000,
+        Format: 'ebook',
+        ISBN: '9780345339683',
+        Description: 'An enchanting tale of Bilbo Baggins, a hobbit who embarks on a grand adventure to reclaim treasure guarded by the fearsome dragon Smaug.',
+        Status: 'New',
+        Location: 'Sena Comercio y Turismo',
+        Publication_Date: 'September 21, 1937',
+        Editorial: 'Houghton Mifflin Harcourt',
+        Pages: 310
+      },
+
+      {
+        Title: 'The Lord of the Rings',
+        Author: 'J.R.R. Tolkien',
+        Genre: 'Fantasy, Epic',
+        Language: 'English',
+        Price: 30000,
+        Format: 'ebook',
+        ISBN: '9780618640157',
+        Descrition: 'A sweeping epic that follows Frodo Baggins and his companions on a perilous quest to destroy the One Ring and defeat the dark lord Sauron.',
+        Status: 'New',
+        Location: 'Sena Comercio y Turismo',
+        Publication_Date: 'October 20, 1955',
+        Editorial: 'Houghton Mifflin Harcourt',
+        Pages: 1178
+      },
+
+
+      {
+        Title: 'Alice"s Adventures in Wonderland',
+        Author: 'Lewis Carroll',
+        Genre: 'Fantasy, Children\'s Literature',
+        Language: 'English',
+        Price: 20000,
+        Format: 'ebook',
+        ISBN: '9780141439761',
+        Description: 'A whimsical journey into a fantastical world filled with peculiar creatures and nonsensical adventures, enjoyed by readers of all ages.',
+        Status: 'New',
+        Location: 'Sena Comercio y Turismo',
+        Publication_Date: 'July 4, 1865',
+        Editorial: 'Penguin Classics',
+        Pages: 176
+      },
+
+
+      {
+        Title: 'Cien años de soledad',
+        Author: 'Gabriel García Márquez',
+        Genre: 'Magical Realism, Literary Fiction',
+        Language: 'Spanish',
+        Price: 100000,
+        Format: 'ebook',
+        ISBN: '9780307474728',
+        Description: 'A landmark novel in Latin American literature, "Cien años de soledad" tells the multi-generational story of the Buendía family in the fictional town of Macondo, blending elements of magic and reality to create a rich tapestry of Latin American history and culture',
+        Status: 'New',
+        Location: 'Sena Comercio y Turismo',
+        Publication_Date: 'May 30, 1967',
+        Editorial: 'Vintage Español',
+        Pages: 417
+      },
+
+      {
+        Title: 'The Alchemist',
+        Author: 'Paulo Coelho',
+        Genre: 'Fiction, Allegory',
+        Language: 'English',
+        Price: 20000,
+        Format: 'ebook',
+        ISBN: '9780062315007',
+        Description: 'A philosophical novel about a young Andalusian shepherd named Santiago who sets out on a journey to discover his Personal Legend, encountering various challenges and learning profound lessons about life, destiny, and the importance of following one\'s dreams.',
+        Status: 'New',
+        Location: 'Sena Comercio y Turismo',
+        Publication_Date: '1988',
+        Editorial: 'HarperOne',
+        Pages: 208
+      },
+
+
+      {
+        Title: 'The Catcher in the Rye',
+        Author: 'J.D. Salinger',
+        Genre: 'Fiction, Coming-of-Age',
+        Language: 'English',
+        Price: 20000,
+        Format: 'ebook',
+        ISBN: '9780316769488',
+        Description: 'A classic coming-of-age novel that follows Holden Caulfield, a disenchanted teenager, as he navigates the complexities of adolescence and society in New York City.',
+        Status: 'New',
+        Location: 'Sena Comercio y Turismo',
+        Publication_Date: 'July 16, 1951',
+        Editorial: 'Little, Brown and Company',
+        Pages: 224
+      },
+
+
+      {
+        Title: 'Love in the Time of Cholera',
+        Author: 'Gabriel García Márquez',
+        Genre: 'Magical Realism, Romance',
+        Language: 'Spanish',
+        Price: 70000,
+        Format: 'ebook',
+        ISBN: '9780307389732',
+        Description: 'A captivating tale of unrequited love, enduring passion, and the passage of time, set in a Caribbean port town and spanning over 50 years of Colombian history.',
+        Status: 'New',
+        Location: 'Sena Comercio y Turismo',
+        Publication_Date: '1985',
+        Editorial: 'Vintage Español',
+        Pages: 368
+      },
+
+
+      
+
+  
+  ];
